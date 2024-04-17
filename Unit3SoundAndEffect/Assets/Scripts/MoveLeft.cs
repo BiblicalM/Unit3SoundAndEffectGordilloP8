fac9e675ac2f13542f34d368444fc8aa2e3e8c97
Maximiliano Gordillo <724910@student.dusd.net>
2024-04-17 15:20:06 -0700

    a lot of stuff
    background repeats properly and gameover stops everything and obstacles get deleted when they go past a certain point and the player has a run animation through the game.

--- Unit3SoundAndEffect/Assets/Scripts/MoveLeft.cs
+++ Unit3SoundAndEffect/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController playerContrrollerScript;
+    private float leftBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerContrrollerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!playerContrrollerScript.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Unit3SoundAndEffect/Assets/Scripts/SpawnManager.cs
+++ Unit3SoundAndEffect/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPosition = new Vector3(25, 1, -1);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
@@ -23,6 +25,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        }
+        
     }
 }
